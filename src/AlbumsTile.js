e7fa2f866c7d6380ff5d2fbc83fa1da96878a4c7
Jacob Marthaller <marthaller.jr@gmail.com>
2021-08-01 11:17:35 -0700

    Toggle update Album title form on tile

--- src/AlbumsTile.js
+++ src/AlbumsTile.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 function AlbumsTile({ title, id, onDeleteAlbum }) {
+  const [updateAlbumStatus, setUpdateAlbumStatus] = useState(false);
+  const [updateAlbumTitle, setUpdateAlbumTitle] = useState("");
+
+
+  async function handleUpdateSubmit(e) {
+    // e.preventDefault();
+    // const newAlbumToPost = {
+    //   title: newAlbumTitle,
+    //   artist_id: 300,
+    // };
+    // const response = await fetch(`http://localhost:3001/albums`, {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json",
+    //     },
+    //     body: JSON.stringify(newAlbumToPost),
+    // });
+    // const jsonify = await response.json();
+    // onAddAlbumToList(jsonify);
+    // e.target.reset();
+  }
+
+  const toggleUpdate = () => {
+    setUpdateAlbumStatus(!updateAlbumStatus)
+  }
 
     function handleDeleteReview() {
         fetch(`http://localhost:3001/albums/${id}`, {
@@ -16,8 +42,29 @@ function AlbumsTile({ title, id, onDeleteAlbum }) {
 
     return (
             <div className="album-card" >
-                <h1>{title}</h1>
-                <h3 onClick={handleDeleteReview}>X</h3>
+                <h1>Album: {title}</h1>
+                <button onClick={handleDeleteReview}>X</button>
+                <button onClick={toggleUpdate}>Update Album Title</button>
+                {updateAlbumStatus ?
+                  <form className="update-album-form-input" onSubmit={handleUpdateSubmit}>
+                  <input
+                    className="update-album-form-input"
+                    type="text"
+                    id="title"
+                    value={updateAlbumTitle}
+                    placeholder="New Title"
+                    onChange={(e) => setUpdateAlbumTitle(e.target.value)}
+                  />
+                  <br></br>
+                  <input
+                    className="submit-update-album-btn"
+                    type="submit"
+                    value="Submit New Album"
+                />
+              </form>
+                
+                : null
+                }
             </div>
     )
 }
